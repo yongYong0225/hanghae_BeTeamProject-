e6bafc1319e02650fdeaccdbac71f09e215f70e7
dorothyah <118116889+dorothyah@users.noreply.github.com>
2022-12-12 21:28:54 +0900

    홍게에에엥
    가버렷

---
+++ BeTeamProject/src/main/java/com/sparta/myboard/service/PostService.java
@@ -0,0 +1,85 @@
+package com.sparta.myboard.service;
+
+import com.sparta.myboard.dto.PostDeleteResponseDto;
+import com.sparta.myboard.dto.PostRequestDto;
+import com.sparta.myboard.dto.PostResponseDto;
+import com.sparta.myboard.entity.Post;
+import com.sparta.myboard.entity.User;
+import com.sparta.myboard.entity.UserRoleEnum;
+import com.sparta.myboard.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    //@Transactional -> acid캐시를 깨지지 않게 해줌
+    @Transactional(readOnly = true)
+    public List<PostResponseDto> getPosts() { //#2 메서드 실행
+        return //#7 컨트롤러로 데이터를 리턴해준다
+                postRepository.findAllPostByOrderByCreatedAtDesc() //#3 레포지토리가 데이터를 찾아옴
+                        .stream() //#4 stream 클래스로 찾아온 데이터를 변환시켜줌
+                        .map(PostResponseDto::new) //#5 PostResponseDto에 레포지토리가 찾아온 데이터를 할당시켜줌
+                        .collect(Collectors.toList()); //#6 PostResponseDto를 리스트에 담아줌
+    }
+
+    @Transactional
+    public PostResponseDto creatPost(PostRequestDto requestDto, User user) {
+        //토큰 검사
+
+        Post post = new Post(requestDto, user.getUsername());
+        post = postRepository.save(post);
+        post.setUser(user);
+        return new PostResponseDto(post);
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponseDto getPost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        return new PostResponseDto(post);
+    }
+
+    @Transactional
+    public PostResponseDto updatePost(Long id, PostRequestDto requestDto, User user) {
+
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        if (userRoleEnum == UserRoleEnum.ADMIN) {
+            Post post = postRepository.findById(id).get();
+            post.update(requestDto, user.getUsername());
+            return new PostResponseDto(post);
+        } else if (postRepository.existsByIdAndUsername(id, user.getUsername()) && userRoleEnum == UserRoleEnum.USER) {
+            Post post = postRepository.findById(id).get();
+            post.update(requestDto, user.getUsername());
+            return new PostResponseDto(post);
+        } else {
+            throw new IllegalArgumentException("게시글을 삭제할 수 없습니다.");
+        }
+    }
+
+    @Transactional
+    public PostDeleteResponseDto deletePost(Long id, User user) {
+
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        //유효한 토큰일 경우 삭제
+        if (user.getRole().equals(UserRoleEnum.ADMIN)) {
+            postRepository.deleteById(id);
+            return new PostDeleteResponseDto("게시글 삭제 성공", HttpStatus.OK.value());
+        } else if (postRepository.existsByIdAndUsername(id, user.getUsername()) && userRoleEnum == UserRoleEnum.USER){
+            postRepository.deleteById(id);
+            return new PostDeleteResponseDto("게시글 삭제 성공", HttpStatus.OK.value());
+        } else {
+            throw new IllegalArgumentException("게시글 삭제 실패");
+        }
+    }
+}
---
+++ BeTeamProject/src/main/java/com/sparta/myboard/service/UserService.java
@@ -0,0 +1,79 @@
+package com.sparta.myboard.service;
+
+import com.sparta.myboard.dto.LoginRequestDto;
+import com.sparta.myboard.dto.LoginResponseDto;
+import com.sparta.myboard.dto.SignupRequestDto;
+import com.sparta.myboard.dto.SignupResponseDto;
+import com.sparta.myboard.entity.User;
+import com.sparta.myboard.entity.UserRoleEnum;
+import com.sparta.myboard.jwt.JwtUtil;
+import com.sparta.myboard.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    // ADMIN_TOKEN
+    private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    @Transactional
+    public SignupResponseDto signup(SignupRequestDto signupRequestDto){
+        //받아온 유저네임과 패스워드를 변수에 저장
+        String username = signupRequestDto.getUsername();
+        String password = passwordEncoder.encode(signupRequestDto.getPassword());
+
+        //회원 중복 확인, 받아온 값이 유저레포지토리에 있는지 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) { //존재하는 것을 찾았다면 에러처리
+            throw new IllegalArgumentException("이미 등록된 ID 입니다.");
+        }
+
+        // 사용자 ROLE 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (signupRequestDto.isAdmin()) {
+            if (!signupRequestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        //user 객체에 두 값을 저장
+        User user = new User(username, password, role);
+        userRepository.save(user);
+
+        return new SignupResponseDto("회원가입 성공", HttpStatus.OK.value());
+    }
+
+    @Transactional(readOnly = true)
+    public LoginResponseDto login(LoginRequestDto loginRequestDto, HttpServletResponse response){
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+
+        //사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        //비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+
+        return new LoginResponseDto("로그인 성공", HttpStatus.OK.value());
+    }
+
+}

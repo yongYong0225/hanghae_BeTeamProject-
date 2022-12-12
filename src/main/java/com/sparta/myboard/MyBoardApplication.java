785ca5704b025a48d0832f0d143084266f6170b1
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-12 12:11:37 +0900

    실행테스트 주석

--- src/main/java/com/sparta/myboard/MyBoardApplication.java
+++ src/main/java/com/sparta/myboard/MyBoardApplication.java
@@ -1,5 +1,5 @@
 package com.sparta.myboard;
-
+//실행 테스트 확인
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
